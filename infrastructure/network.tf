771c25a4ae2db2975a2a270601b4a6aaa43ba059
ArerepadeB <aredelulu@gmail.com>
2022-03-30 01:25:08 -0500

    added natgw to access internet

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "main-public" {
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, { Name = "main-public", Company = "cloudreach" })
 }
 
 resource "aws_subnet" "main-public-1" {
@@ -22,21 +22,21 @@ resource "aws_subnet" "main-public-1" {
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, { Name = "main-public-1", Company = "cloudreach" })
 }
 ////subnet - private
 resource "aws_subnet" "main-private-1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  tags              = local.common_tags
+  tags = merge(local.common_tags, { Name = "main-private-1", Company = "cloudreach" })
 }
 
 resource "aws_subnet" "main-private-2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1c"
-  tags              = local.common_tags
+  tags = merge(local.common_tags, { Name = "main-private-2", Company = "cloudreach" })
 }
 
 ///igw
@@ -54,7 +54,7 @@ resource "aws_route_table" "rtb" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, { Name = "cloudreach", Company = "cloudreach" })
 }
 
 ///subnet association
@@ -70,12 +70,12 @@ resource "aws_route_table_association" "rtb-public-1" {
 
 resource "aws_route_table_association" "rtb-private-1" {
   subnet_id      = aws_subnet.main-private-1.id
-  route_table_id = aws_route_table.rtb.id
+  route_table_id = aws_route_table.public-nat.id
 }
 
 resource "aws_route_table_association" "rtb-private-2" {
   subnet_id      = aws_subnet.main-private-2.id
-  route_table_id = aws_route_table.rtb.id
+  route_table_id = aws_route_table.public-nat.id
 }
 
 /////sg
@@ -165,4 +165,32 @@ resource "aws_security_group" "alb" {
   tags = {
     Name = "alb-sg"
   }
+}
+
+resource "aws_eip" "cloudreach-eip" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "cloureach-nat" {
+  allocation_id = aws_eip.cloudreach-eip.id
+  subnet_id     = aws_subnet.main-public.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  # depends_on = [aws_internet_gateway.cloureach-nat]
+}
+////route natgateway
+resource "aws_route_table" "public-nat" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.cloureach-nat.id
+  }
+
+  tags = merge(local.common_tags, { Name = "public-nat", Company = "cloudreach" })
 }
